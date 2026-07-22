a8e34419d726bc0683a1ff4e1d42121199009dbc
Zsofia Magyar <zsofia.magyar@nttdata.com>
2026-07-22 12:47:42 +0000

    comment teszt

--- webapp/ext/controller/CustomActions.controller.js
+++ webapp/ext/controller/CustomActions.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
   "sap/ui/core/mvc/ControllerExtension"
 ], function(ControllerExtension) {
   "use strict";
-
+//lkjsndflkgngf
   /*return ControllerExtension.extend("zmes.zmesppoplist.ext.controller.CustomActions", {
     override: {
       editFlow: {
